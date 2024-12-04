39cf9d331166063ed4ddd6a94d2a15f97c915763
Michael Fleming <mikahl171@gmail.com>
2024-12-03 20:36:00 -0600

    boilerplate code for the auth files

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,6 +2,22 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  // and return the user data and token
+  const response = await fetch('/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userInfo)
+  });
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  const data = await response.json();
+  return data;
+
 }
 
 
---
+++ server/src/seeds/routes/auth-routes.ts
@@ -0,0 +1,23 @@
+import { Router, Request, Response } from 'express';
+import { User } from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+export const login = async (req: Request, res: Response) => {
+  // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+  const user = await User.findOne({ where: { username } });
+  if (user && await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ username }, process.env.TOKEN_SECRET as string);
+    res.json({ token });
+  } else {
+    res.status(400).json({ message: 'Invalid username or password' });
+  }
+};
+
+const router = Router();
+
+// POST /login - Login a user
+router.post('/login', login);
+
+export default router;
